58b3807c2244c80bef7637040bf52ef9cbadff91
Oscar-Espinoza <oscardavid2094@gmail.com>
2023-03-20 15:50:29 -0300

    linter errors fixed

--- menu.rb
+++ menu.rb
@@ -6,6 +6,25 @@ class Menu
     @app = app
   end
 
+  def menu_actions(choice)
+    case choice
+    when 1
+      @app.list_all_books
+    when 2
+      @app.list_all_people
+    when 3
+      create_person_menu
+    when 4
+      create_book_menu
+    when 5
+      @app.create_rental
+    when 6
+      list_rentals_menu
+    when 7
+      puts 'Goodbye!'
+    end
+  end
+
   def display_menu
     loop do
       puts "\nPlease choose an option by entering a number:"
@@ -19,24 +38,11 @@ class Menu
 
       choice = gets.chomp.to_i
 
-      case choice
-      when 1
-        @app.list_all_books
-      when 2
-        @app.list_all_people
-      when 3
-        create_person_menu
-      when 4
-        create_book_menu
-      when 5
-        @app.create_rental
-      when 6
-        list_rentals_menu
-      when 7
-        puts 'Goodbye!'
-        break
-      else
+      if choice < 1 || choice > 7
         puts 'Invalid option. Please choose a number between 1 and 7.'
+      else
+        menu_actions(choice)
+        break if choice == 7
       end
     end
   end
@@ -45,24 +51,12 @@ class Menu
 
   def create_person_menu
     loop do
-      puts 'Do you want to create a student (1) or a teacher (2)? [Input the number]: '
-      type = gets.chomp
-      puts 'Enter name:'
-      name = gets.chomp
-      puts 'Enter age:'
-      age = gets.chomp.to_i
-
+      type, name, age = create_person_display
       if type == '1'
-        puts 'Has parent permission? [Y/N]: '
-        permission = gets.chomp
-        permission = true if %w[Y y].include?(permission)
-        permission = false if %w[N n].include?(permission)
-        @app.create_person(type, age, name, nil, parent_permission: permission)
+        create_person_with_parent_permission(type, name, age)
         break
       elsif type == '2'
-        puts 'Enter specialization:'
-        specialization = gets.chomp
-        @app.create_person(type, age, name, specialization)
+        create_person_with_specialization(type, name, age)
         break
       else
         puts 'Invalid input, please enter 1 or 2.'
@@ -70,47 +64,28 @@ class Menu
     end
   end
 
-  def create_book_menu
-    puts 'Enter book title:'
-    title = gets.chomp
-    puts 'Enter book author:'
-    author = gets.chomp
-    @app.create_book(title, author)
+  def create_person_display
+    puts 'Do you want to create a student (1) or a teacher (2)? [Input the number]: '
+    type = gets.chomp
+    puts 'Enter name:'
+    name = gets.chomp
+    puts 'Enter age:'
+    age = gets.chomp.to_i
+    [type, name, age]
   end
 
-  def list_rentals_menu
-    puts 'Enter person ID:'
-    person_id = gets.chomp.to_i
-    @app.list_rentals_by_person_id(person_id)
+  def create_person_with_parent_permission(type, name, age)
+    puts 'Has parent permission? [Y/N]: '
+    permission = gets.chomp
+    permission = true if %w[Y y].include?(permission)
+    permission = false if %w[N n].include?(permission)
+    @app.create_person(type, age, name, nil, parent_permission: permission)
   end
-  private
 
-  def create_person_menu
-    loop do
-      puts 'Do you want to create a student (1) or a teacher (2)? [Input the number]: '
-      type = gets.chomp
-      puts 'Enter name:'
-      name = gets.chomp
-      puts 'Enter age:'
-      age = gets.chomp.to_i
-
-      case type
-      when '1'
-        puts 'Has parent permission? [Y/N]: '
-        permission = gets.chomp
-        permission = true if %w[Y y].include?(permission)
-        permission = false if %w[N n].include?(permission)
-        @app.create_person(type, age, name, nil, parent_permission: permission)
-        break
-      when '2'
-        puts 'Enter specialization:'
-        specialization = gets.chomp
-        @app.create_person(type, age, name, specialization)
-        break
-      else
-        puts 'Invalid input, please enter 1 or 2.'
-      end
-    end
+  def create_person_with_specialization(type, name, age)
+    puts 'Enter specialization:'
+    specialization = gets.chomp
+    @app.create_person(type, age, name, specialization)
   end
 
   def create_book_menu
